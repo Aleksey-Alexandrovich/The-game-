55c0c7f6c3e62d1be77d0a2dd4de0f9125f72d0f
Aleksey <alexeyegorov1987@mail.ru>
2023-01-12 00:36:26 +0300

    add the game

--- game01.js
+++ game01.js
@@ -1,30 +1,42 @@
+"use strict";
 
 
+	
 const number = Math.floor(Math.random() * 100) + 1;
 console.log(number);
+let count = 3;
 
 
 document.getElementById('check').onclick = function () {
-	let usernum = document.getElementById('mynum').value;
-	usernum = parseInt(usernum);
-	let out = document.getElementById('out');
-	if(usernum == number ) {
-		out.innerHTML = 'Правильно!';
-	}
-	if (usernum > number) {
-		out.innerHTML = 'Меньше!';
-	}
-	if (usernum < number) {
-		out.innerHTML = 'Больше!';
-	}
+		if(count > 0) {
+				let usernum = +document.getElementById('mynum').value;
+				usernum = parseInt(usernum);
+				let out = document.getElementById('out');
+				if(usernum == number ) {
+					out.innerHTML = 'Правильно!';
+				}
+				if (usernum > number) {
+					out.innerHTML = 'Меньше!';
+				}
+				if (usernum < number) {
+					out.innerHTML = 'Больше!';
+				}
 
-	if(!parseInt(usernum)) {
-		out.innerHTML = 'Введи число!';
-	}
-	if(usernum === null || usernum === "") {
-		out.innerHTML = 'Oтмена';
-	}
-	else {
-		out.innerHTML = 'Отмена!';
-	}
+				if(!parseInt(usernum)) {
+					out.innerHTML = 'Введи число!';
+				}
+
+				if(usernum == null || usernum == "") {
+					out.innerHTML = 'Отмена';
+				}
+				count = count - 1;
+				document.getElementById('count').innerHTML = `Осталось попыток: ${count}`;
+		}		
+		else {
+			alert('Попытки закончены, вы проиграли. Страница будет перезагружена');
+			location.reload();
+		}
+
 }
+
+
