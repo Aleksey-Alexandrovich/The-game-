ad31eb4d0e06645c0e216f32c5c38e447792578f
Alexey <84179953+Aleksey-Alexandrovich@users.noreply.github.com>
2023-02-19 13:49:32 +0300

    add the game

--- game01.js
+++ game01.js
@@ -1,42 +1,72 @@
+
+
 "use strict";
+// Задание №1
+// Задача #1
+// Переписать игровой бот для игры "Угадай число"
+// Функционал остаётся прежний, но вместо цикла (while или for) используйте рекурсию
+{
+	let randomNum = parseInt(Math.random() * 100);
 
-
+	function isNum(num) {
+		return !isNaN(parseFloat(num)) && isFinite(num);
+	}
 	
-const number = Math.floor(Math.random() * 100) + 1;
-console.log(number);
-let count = 3;
-
-
-document.getElementById('check').onclick = function () {
-		if(count > 0) {
-				let usernum = +document.getElementById('mynum').value;
-				usernum = parseInt(usernum);
-				let out = document.getElementById('out');
-				if(usernum == number ) {
-					out.innerHTML = 'Правильно!';
-				}
-				if (usernum > number) {
-					out.innerHTML = 'Меньше!';
-				}
-				if (usernum < number) {
-					out.innerHTML = 'Больше!';
-				}
-
-				if(!parseInt(usernum)) {
-					out.innerHTML = 'Введи число!';
-				}
-
-				if(usernum == null || usernum == "") {
-					out.innerHTML = 'Отмена';
-				}
-				count = count - 1;
-				document.getElementById('count').innerHTML = `Осталось попыток: ${count}`;
-		}		
-		else {
-			alert('Попытки закончены, вы проиграли. Страница будет перезагружена');
-			location.reload();
+	function guessNumber() {
+		let userNum = prompt('Введите число');
+		if (isNum(userNum)) {
+			userNum = +userNum;
+	
+			if (userNum > randomNum) {
+				alert('Вы ввели большее число');
+			} else if (userNum < randomNum) {
+				alert('Вы ввели меньшее число');
+			} else if (userNum === randomNum) {
+				alert('Верно, Вы угадали загаданное число!');
+				return userNum;
+			}
+		} else {
+			alert('Можно вводить только числа');
 		}
+		return guessNumber();
+	}
+	
+	console.log(guessNumber());
 
 }
+
+
+
+///Игра с рекурсией
+// Задание №2
+// Напишите рекурсивную функцию,
+// которая принимает один параметр массив,  генерирует целое число от 0 до 10
+// включительно и добавляет его в массив.
+// Каждый раз после добавления нового числа проверяет сумму элементов массива,
+//  если она меньше 50 запускается снова передавая себе массив. Если сумма больше
+// 50-ти, то функция возвращает этот массив.
+
+function generate (newArray = []) {
+	let randomNumber =  Math.floor(Math.random() * 10) + 1;
+	console.log(randomNumber)
+	newArray.push(randomNumber);
+	let total = newArray.reduce((acc, sum) => acc + sum, 0);   
+	console.log('total: ', total);
+
+	if(total < 50)  {
+		return generate(newArray);
+	} else if(total > 50) {
+		return newArray;
+
+	}
+	
+};
+
+console.log(generate());
+
+
+///////////////////////////////////////////////////////////////////////
+
+
 
 
